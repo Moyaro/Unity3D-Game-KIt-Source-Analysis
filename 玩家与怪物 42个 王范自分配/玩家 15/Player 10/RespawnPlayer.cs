65b9aee55e14243bc0cc2351a26fec32dfd355b6
diaoerlangdang17 <diaoerlangdang17@gmail.com>
2020-05-17 10:22:48 +0800

    update 王寿岩

--- 玩家与怪物 42个 王范自分配/玩家 15/Player 10/RespawnPlayer.cs
+++ 玩家与怪物 42个 王范自分配/玩家 15/Player 10/RespawnPlayer.cs
@@ -4,9 +4,9 @@ namespace Gamekit3D.GameCommands
     {
         public Gamekit3D.PlayerController player;
 
-        public override void PerformInteraction()
+        public override void PerformInteraction()//重写：类的继承时发生，在子类中重新定义父类中的方法，子类中的方法和父类的方法是一样的，即方法名，参数，返回值类型都相同。原文链接：https://blog.csdn.net/kj297296053/java/article/details/8213550
         {
-            player.Respawn();
+            player.Respawn();//翻译：复位。      并在代码EllenRespawnEffect中引用
         }
     }
 }
