6084fd2beec8c995c14adc3f43dc24ef013d52b2
arikes <1933958015@qq.com>
2020-05-29 22:09:22 +0800

    no message

--- 系统(1) 42/Core 16/InteractionCollision.cs
+++ 系统(1) 42/Core 16/InteractionCollision.cs
@@ -6,20 +6,20 @@ using UnityEngine.Events;
 namespace Gamekit3D
 {
     [RequireComponent(typeof(Collider))]
-    public class InteractionCollision : MonoBehaviour
+    public class InteractionCollision : MonoBehaviour//碰撞互动
     {
-        public LayerMask layers;
-        public UnityEvent OnCollision;
+        public LayerMask layers;//当前所在层
+        public UnityEvent OnCollision;//碰撞事件
 
-        void Reset()
+        void Reset()//重置
         {
-            layers = LayerMask.NameToLayer("Everything");
+            layers = LayerMask.NameToLayer("Everything");//将当前层设置在Everything层
         }
 
-        void OnCollisionEnter(Collision c)
+        void OnCollisionEnter(Collision c)//碰撞开始时
         {
-            Debug.Log(c);
-            if (0 != (layers.value & 1 << c.transform.gameObject.layer))
+            Debug.Log(c);//在控制台打印出碰撞物的信息
+            if (0 != (layers.value & 1 << c.transform.gameObject.layer))//如果碰撞了当前层就将碰撞事件唤醒
             {
                 OnCollision.Invoke();
             }
@@ -27,12 +27,12 @@ namespace Gamekit3D
 
         void OnDrawGizmos()
         {
-            Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
+            Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);//在当前位置绘制一个图标，图标名称为InteractionTrigger
         }
 
         void OnDrawGizmosSelected()
         {
-            //need to inspect events and draw arrows to relevant gameObjects.
+            //需要检查事件并画出相关游戏对象的箭头。 need to inspect events and draw arrows to relevant gameObjects.
         }
 
     } 
