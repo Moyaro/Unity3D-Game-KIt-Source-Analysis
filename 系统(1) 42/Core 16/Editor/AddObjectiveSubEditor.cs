a39eaa81fcd788f7680453078752990ac9a4953a
banyuanpeng <945649644@qq.com>
2020-05-31 07:27:35 +0800

    no message

--- 系统(1) 42/Core 16/Editor/AddObjectiveSubEditor.cs
+++ 系统(1) 42/Core 16/Editor/AddObjectiveSubEditor.cs
@@ -4,32 +4,32 @@ using UnityEditor;
 
 namespace Gamekit3D
 {
-    public class AddObjectiveSubEditor : SubEditor<ScenarioController>
+    public class AddObjectiveSubEditor : SubEditor<ScenarioController>//增加物体的子编辑器
     {
-        string newObjectiveName = "";
-        bool showAdd = false;
-        string addError = "";
+        string newObjectiveName = "";//新物体名字
+        bool showAdd = false;//Add按钮的显示和隐藏
+        string addError = "";//显示错误
 
         public override void OnInspectorGUI(ScenarioController sc)
         {
 
-            if (!showAdd && GUILayout.Button("Add New Objective"))
+            if (!showAdd && GUILayout.Button("Add New Objective")) //GUILayout无需设定显示区域，系统会自动帮我们计算控件显示区域，并保证不重叠和影响里面的显示效果。 https://blog.csdn.net/u013289188/article/details/29618897
             {
                 showAdd = true;
-                GUI.FocusControl("ObjectiveName");
+                GUI.FocusControl("ObjectiveName"); //移动键盘焦点到ObjectiveName控件。
             }
             if (showAdd)
-            {
-                using (new GUILayout.HorizontalScope())
+            {                      //using 用于定义一个范围，在此范围的末尾将释放对象
+                using (new GUILayout.HorizontalScope()/*所有放在这里面的元素都会被水平排列放置*/)
                 {
-                    GUI.SetNextControlName("ObjectiveName");
-                    GUILayout.Label("Objective Name");
-                    newObjectiveName = EditorGUILayout.TextField(newObjectiveName).ToUpper();
-                    using (new EditorGUI.DisabledScope(newObjectiveName == ""))
+                    GUI.SetNextControlName("ObjectiveName");//设置下一个控件的名字
+                    GUILayout.Label("Objective Name");//创建一个自动布局的标签。
+                    newObjectiveName = EditorGUILayout.TextField(newObjectiveName).ToUpper();//TextField创建一个单行文本字段，用户可以编辑其中的字符串，toupper：将小写英文字母改为对应的大写字母
+                    using (new EditorGUI.DisabledScope(newObjectiveName == ""))//如果此时还没有新物体的名字，就增加物体的名字。disabledscope:设置一个范围并创建一个组。
                     {
                         if (GUILayout.Button("Add"))
                         {
-                            if (sc.AddObjective(newObjectiveName, 1))
+                            if (sc.AddObjective(newObjectiveName, 1))//参数：增加物体的名字和数量。如果增加成功，将所有数据进行重置
                             {
                                 Close();
                             }
@@ -42,9 +42,9 @@ namespace Gamekit3D
                 }
                 if (addError != "")
                 {
-                    EditorGUILayout.HelpBox(addError, MessageType.Error);
+                    EditorGUILayout.HelpBox(addError, MessageType.Error);//将错误类型设置为error，并在helpbox类打出提示信息  https://www.cnblogs.com/backlighting/p/5061576.html
                 }
-                if (Event.current.isKey && Event.current.keyCode == KeyCode.Escape)
+                if (Event.current.isKey && Event.current.keyCode == KeyCode.Escape)//如果此时在用键盘并且键盘所按键为ESC
                 {
                     Close();
                 }
@@ -57,8 +57,11 @@ namespace Gamekit3D
             addError = "";
             showAdd = false;
             EditorGUIUtility.editingTextField = false;
-            Repaint();
+            Repaint();//重绘界面控件
         }
 
     } 
 }
+
+
+//关于GUILayout类的函数的参照表 https://blog.csdn.net/likendsl/article/details/50254827
--- 系统(1) 42/Core 16/Editor/SubEditor.cs
+++ 系统(1) 42/Core 16/Editor/SubEditor.cs
@@ -2,32 +2,35 @@ using UnityEditor;
 
 namespace Gamekit3D
 {
+	//该类主要用于对画面的重置对现有委托进行使用并清除
+        //抽象类：子编辑器，不能生成实例  关于abstract的详细说明https://blog.csdn.net/yk_lin/article/details/83469094
+        //泛型<T> https://www.sogou.com/link?url=hedJjaC291OB0PrGj_c3jJzmXqp0xreSchXwq6KZroyroHKd7_5kXCbc-iPH6wf8jzYWkh2yY3ynXTW0b-n0YQ..
     public abstract class SubEditor<T>
     {
         public abstract void OnInspectorGUI(T instance);
 
-        public void Init(Editor editor)
+        public void Init(Editor editor)//获得当前编辑器
         {
             this.editor = editor;
         }
 
         public void Update()
         {
-            if (defer != null) defer();
+            if (defer != null) defer();//如果当前委托不为空，就运行当前委托，然后再次设为空
             defer = null;
         }
 
-        protected void Defer(System.Action fn)
+        protected void Defer(System.Action fn)//在委托中添加要执行的方法
         {
             defer += fn;
         }
 
-        protected void Repaint()
+        protected void Repaint()//用来重绘界面所有的控件
         {
             editor.Repaint();
         }
 
         Editor editor;
-        System.Action defer;
+        System.Action defer;//一种特殊委托  https://blog.csdn.net/qq_38187606/article/details/78928876
     } 
 }
